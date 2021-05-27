a63a7e89b674f5e9068a037e208e428f6fbc9fa9
unknown <POPLEDWARF@GMAIL.COM>
2021-05-27 08:05:44 -0700

    adding animators and starter scene

---
+++ Dragno/Assets/Scripts/Enemy.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float enemyMoveSpeed = 2f;
+    [SerializeField] public float enemyHealth = 1;
+
+
+    Rigidbody2D myRigidBody;
+    CircleCollider2D myCircleCollider2D;
+
+    Vector3 enemyScale;
+    float enemyScaleX;
+
+
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        enemyScale = transform.localScale;
+        enemyScaleX = enemyScale.x;
+    }
+
+    void Update()
+    {
+        if (isFacingRight())
+        {
+            myRigidBody.velocity = new Vector2(-enemyMoveSpeed, 0);
+        }
+        else
+        {
+            myRigidBody.velocity = new Vector2(enemyMoveSpeed, 0);
+        }
+
+    }
+
+    bool isFacingRight()
+    {
+        return transform.localScale.x > 0;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    // make sure to set enemy box collider slightly in the ground for this to work.
+    {
+        if (isFacingRight())
+        {
+            enemyScale.x = -enemyScaleX;
+        }
+        else
+        {
+            enemyScale.x = enemyScaleX;
+        }
+        transform.localScale = enemyScale;
+    }
+
+    private void Die()
+    {
+        if (enemyHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+        //if (myCircleCollider2D.IsTouchingLayers(LayerMask.GetMask("Player", "Place Holder For Player Weapon Mask"))) // add mask for player weapons later
+        //{
+        //    Destroy(gameObject);
+        //}
+    }
+}
---
+++ Dragno/Assets/Scripts/Player Movement.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float playerRunSpeed = 5f;
+    [SerializeField] float playerJumpSpeed = 10f;
+
+    public bool isAlive = true;
+
+    Rigidbody2D myRigidBody;
+    Animator myAnimator;
+    CapsuleCollider2D myCapsuleCollider2D;
+    BoxCollider2D myFeet;
+    CircleCollider2D myWeapon; // make sure to set this on its own layer //
+
+    Vector3 characterScale;
+    float characterScaleX;
+
+    public void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myCapsuleCollider2D = GetComponent<CapsuleCollider2D>();
+        myFeet = GetComponent<BoxCollider2D>();
+        myWeapon = GetComponent<CircleCollider2D>();
+        characterScale = transform.localScale;
+        characterScaleX = characterScale.x;
+    }
+
+    public void Update()
+    {
+        if (!isAlive) { return; }
+        Run();
+        Jump();
+        Attack();
+        TakeDamage();
+        TurnAround();
+    }
+
+    private void Run()
+    {
+        float controlThrow = Input.GetAxis("Horizontal");
+        Vector2 playerVelocity = new Vector2(controlThrow * playerRunSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        //myAnimator.SetBool("Running", playerHasHorizontalSpeed); put back in when we have a running animation
+
+    }
+
+    private void Jump()
+    {
+        if (!myFeet.IsTouchingLayers(LayerMask.GetMask("Foreground"))) { return; }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, playerJumpSpeed);
+            myRigidBody.velocity += jumpVelocityToAdd;
+        }
+    }
+
+    private void Attack()
+    {
+        if (Input.GetMouseButtonDown(0)) //Sword Attack
+        {
+            //StartCoroutine(AttackWithWeapon()); 
+        }
+
+    }
+
+    public void TakeDamage()
+    {
+        if (myCapsuleCollider2D.IsTouchingLayers(LayerMask.GetMask("Enemies", "Hazards")))
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        isAlive = false;
+        FindObjectOfType<GameSession>().ProcessPlayerDeath();
+
+    }
+
+    private void TurnAround()
+    {
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            characterScale.x = -characterScaleX;
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            characterScale.x = characterScaleX;
+        }
+        transform.localScale = characterScale;
+    }
+
+    //private IEnumerator AttackWithWeapon()
+    //{
+    //   myWeapon.enabled = true;
+    // yield return new WaitForSecondsRealtime(1);
+    //myWeapon.enabled = false;
+
+//    }
+
